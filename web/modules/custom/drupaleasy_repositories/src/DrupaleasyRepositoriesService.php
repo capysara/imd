adf0fadf8f8e717b113e6bd9b03969b731365497
Sara Cartee-Kennedy <sara.carteekennedy@bounteous.com>
2022-08-08 15:04:23 -0400

    Added service.

---
+++ web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositoriesService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\drupaleasy_repositories;
+
+/**
+ * Service description.
+ */
+class DrupaleasyRepositoriesService {
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    // @DCG place your code here.
+  }
+
+}
